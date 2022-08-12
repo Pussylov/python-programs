5ce6e863b6e7d243d21848aecb86b759632e24b5
Pussylov <2101030600031@silveroakuni.ac.in>
2022-08-12 15:30:55 +0530

    files

--- files.py
+++ files.py
@@ -1,7 +1,12 @@
-a = open('sample.txt','r')
+# a = open('sample.txt','r')
 # # a = open('sample.txt') by default it used read mode
 # data = a.read(6)# reads first fve character
 # print(data)
 # a.close()
 #  # to read lines we use readline()
 # #  mode are read(r) write(w) append(a) update(+)
+
+
+f=open('this.text','a')
+f.write("hello there")
+f.close()
